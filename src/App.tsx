77de8e32ef5a53d066942a3b3bdb53228cdc1e00
s576air <s576ai@naver.com>
2026-03-20 17:34:18 +0900

    Feat: MainPage 작업

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import CreateRoom from "./pages/CreateRoom";
 import TestPage from './pages/TestPage';
+import MainPage from './pages/MainPage';
 
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<div>홈</div>} />
+        <Route path="/" element={<MainPage />} />
         {/* 방만들기 */}
         <Route path="/create-room" element={<CreateRoom />} />
         <Route path='/test' element={<TestPage />} />

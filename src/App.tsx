d49fcd93717f9b4802cc3c98fe70fcc987cbeb17
WookHub <scw0132@naver.com>
2026-03-27 16:25:13 +0900

    feature/OpenViewBefore에 capsuleEditCheckModal, writeMessageModal 연결
    feature/OpenViewBefore에 capsuleEditCheckModal, writeMessageModal 연결

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MainPage from './pages/MainPage';
 import CreateCapsulePage from "./pages/CreateCapsulePage";
 import CapsulePage from "./pages/CapsulePage";
 import TestPage from './pages/TestPage';
+import Modal from "./shared/components/ui/Modal";
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="/room" element={<CapsulePage />} />
         <Route path='/test' element={<TestPage />} />
       </Routes>
+      <Modal />
     </>
   );
 

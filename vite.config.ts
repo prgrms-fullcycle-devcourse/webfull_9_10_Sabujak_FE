70e0a0d86b51a3f6d101cabe1d9845a5c69502fb
WookHub <scw0132@naver.com>
2026-03-30 12:49:44 +0900

    Revert "백엔드 연결 및 capsule.ts eslint-disable 추가"
    This reverts commit f39bd65a511012086c1e80ef642cadf97ca07ba4.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://webfull-9-10-sabujak-be.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-});
+})

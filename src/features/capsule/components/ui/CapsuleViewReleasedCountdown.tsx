6beacd306f448b033c2326e9023eda7929161113
WookHub <scw0132@naver.com>
2026-04-16 16:05:08 +0900

    fix/오픈후 페이지 멘트 변경

--- src/features/capsule/components/ui/CapsuleViewReleasedCountdown.tsx
+++ src/features/capsule/components/ui/CapsuleViewReleasedCountdown.tsx
@@ -25,7 +25,7 @@ function CountdownRenderer({
 
   return (
     <div className="text-[14px] leading-relaxed">
-      이 타임캡슐은 <NumberRoller value={days} height={height}/>일 <NumberRoller value={hours} height={height}/>시 <NumberRoller value={minutes} height={height}/>분 <NumberRoller value={seconds} height={height}/>초 후에 영구히 사라집니다.
+      D-<NumberRoller value={days} height={height}/> <NumberRoller value={hours} height={height}/>:<NumberRoller value={minutes} height={height}/>:<NumberRoller value={seconds} height={height}/>
     </div>
   );
 }
